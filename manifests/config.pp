3dd965d9719148ef025d35e40e1633285de7f3f8
Andrew Rowson <github@growse.com>
2013-10-28 18:21:30 +0000

    Added file requirement for pdns.d dir to exist

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,13 +13,17 @@
 #
 define powerdns::config($value, $ensure='present') {
 
+  file {'/etc/powerdns/pdns.d':
+      ensure => directory
+  }
+
   file { "/etc/powerdns/pdns.d/${name}.conf":
     ensure  => $ensure,
     owner   => root,
     group   => root,
     mode    => '0700',
     content => "${name}=${value}",
-    require => Class['powerdns::package'],
+    require => [File['/etc/powerdns/pdns.d'],Class['powerdns::package']],
     notify  => Class['powerdns::service'],
   }
 
