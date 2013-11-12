43f7fde9c5109912b1438ed8c60c34a87a92dfe0
Andrew Rowson <github@growse.com>
2013-11-12 12:45:14 +0000

    Sane config now

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,16 +12,21 @@
 #    }
 #
 define powerdns::config($value, $ensure='present') {
+    file {"/etc/powerdns/pdns.conf":
+        ensure  => file,
+        content => template('powerdns/pdns.conf.erb'),
+        require => [File['/etc/powerdns/pdns.d'],Class['powerdns::package']],
+        notify  => Class['powerdns::service'],
+    }
 
-
-  file { "/etc/powerdns/pdns.d/${name}.conf":
-    ensure  => $ensure,
-    owner   => root,
-    group   => root,
-    mode    => '0700',
-    content => "${name}=${value}",
-    require => [File['/etc/powerdns/pdns.d'],Class['powerdns::package']],
-    notify  => Class['powerdns::service'],
-  }
+    file { "/etc/powerdns/pdns.d/${name}.conf":
+        ensure  => $ensure,
+        owner   => root,
+        group   => root,
+        mode    => '0700',
+        content => "${name}=${value}",
+        require => [File['/etc/powerdns/pdns.d'],Class['powerdns::package']],
+        notify  => Class['powerdns::service'],
+    }
 
 }
