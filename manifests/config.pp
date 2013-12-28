e46a4041b9d69cea73e1dfa2a98690854dd8ad60
Andrew Rowson <github@growse.com>
2013-12-28 10:08:59 +0000

    Updated config owner to pdns

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,7 +15,7 @@ define powerdns::config($value, $ensure='present') {
 
     file { "/etc/powerdns/pdns.d/${name}.conf":
         ensure  => $ensure,
-        owner   => root,
+        owner   => pdns,
         group   => root,
         mode    => '0700',
         content => "${name}=${value}",
--- manifests/package.pp
+++ manifests/package.pp
@@ -30,10 +30,12 @@ class powerdns::package(
 
     file {'/etc/powerdns/pdns.d':
         require => Package[$package],
+        owner   => 'pdns',
         ensure  => directory
     }
     file {"/etc/powerdns/pdns.conf":
         ensure  => file,
+        owner   => 'pdns',
         content => template('powerdns/pdns.conf.erb'),
         require => [File['/etc/powerdns/pdns.d'],Package[$package]],
         notify  => Class['powerdns::service'],
